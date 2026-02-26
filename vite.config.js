7e7df19939d3f349da0fb1880718b987e87684c5
tarequlislam332760-collab <tarequlislam332760@gmail.com>
2026-02-25 21:48:27 -0800

    added base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),]
+  plugins: [react(), tailwindcss()],
+  base: './', // এটি যোগ করলে ফাইল খুঁজে পেতে সুবিধা হয়
 })
